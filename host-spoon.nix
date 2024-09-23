2bb972d1e3c96f80e0a93bf37fa64b61bca1fab2
Diogo Correia <diogotc@kth.se>
2024-09-24 00:10:01 +0200

    fix: force enable dhcp

--- host-spoon.nix
+++ host-spoon.nix
@@ -32,12 +32,15 @@
   networking.firewall.allowedTCPPorts = [80];
 
   # Set static IP (mainly for demo purposes)
-  networking.interfaces.enp1s0.ipv4.addresses = [
-    {
-      address = "192.168.122.10";
-      prefixLength = 24;
-    }
-  ];
+  networking.interfaces.enp1s0 = {
+    useDHCP = true; # for gateway and dns
+    ipv4.addresses = [
+      {
+        address = "192.168.122.10";
+        prefixLength = 24;
+      }
+    ];
+  };
 
   system.stateVersion = "24.05";
 }
